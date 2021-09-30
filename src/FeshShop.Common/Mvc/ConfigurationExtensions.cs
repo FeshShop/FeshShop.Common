859b3ce99e1265b126d35d4924d335b65e58add8
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2021-09-30 14:27:03 +0300

    configure startup of mongodb and registeration of services

---
+++ src/FeshShop.Common/Mvc/ConfigurationExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace FeshShop.Common.Mvc
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public static partial class ConfigurationExtensions
+    {
+        public static IServiceCollection AddInitializers(this IServiceCollection services, params Type[] initializers)
+            => initializers == null
+                ? services
+                : services.AddTransient<IStartupInitializer, StartupInitializer>(c =>
+                {
+                    var startupInitializer = new StartupInitializer();
+                    var validInitializers = initializers.Where(t => typeof(IInitializer).IsAssignableFrom(t));
+
+                    foreach (var initializer in validInitializers)
+                    {
+                        startupInitializer.AddInitializer(c.GetService(initializer) as IInitializer);
+                    }
+
+                    return startupInitializer;
+                });
+
+        public static IServiceCollection AddServices(this IServiceCollection services, Assembly assembly)
+            => services.Scan(scan => scan.FromAssemblies(assembly).AddClasses().AsMatchingInterface());
+    }
+}
