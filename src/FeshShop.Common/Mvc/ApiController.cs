41f1ff214c8926811ede02f340946a5869e51a10
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2021-09-30 14:26:31 +0300

    create base api controller

---
+++ src/FeshShop.Common/Mvc/ApiController.cs
@@ -0,0 +1,10 @@
+﻿namespace FeshShop.Common.Mvc
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("[controller]")]
+    public abstract class ApiController : ControllerBase
+    {
+    }
+}
