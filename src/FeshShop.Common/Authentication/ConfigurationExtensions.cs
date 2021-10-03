811439e7dd2e04760b52be1c0953390f4f7d4894
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2021-10-03 19:01:19 +0300

    configure jwt

---
+++ src/FeshShop.Common/Authentication/ConfigurationExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace FeshShop.Common.Authentication
+{
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using System;
+
+    public static partial class ConfigurationExtensions
+    {
+        public static IServiceCollection AddJwt(this IServiceCollection services, IConfiguration configuration) 
+        {
+            var options = configuration
+                .GetSection(nameof(JwtSettings))
+                .Get<JwtSettings>();
+
+            services.AddSingleton(options);
+            services.AddSingleton<IJwtHandler, JwtHandler>();
+
+            return services;
+        }
+
+        public static long ToTimestamp(this DateTime dateTime)
+        {
+            var centuryBegin = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            var expectedDate = dateTime.Subtract(new TimeSpan(centuryBegin.Ticks));
+
+            return expectedDate.Ticks / 10000;
+        }
+    }
+}
---
+++ src/FeshShop.Common/Authentication/JwtSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace FeshShop.Common.Authentication
+{
+    public class JwtSettings
+    {
+        public string SecretKey { get; set; }
+
+        public string Issuer { get; set; }
+
+        public int ExpiryMinutes { get; set; }
+
+        public bool ValidateLifetime { get; set; }
+
+        public bool ValidateAudience { get; set; }
+
+        public string ValidAudience { get; set; }
+    }
+}
