ab87329eedb42a61cf1f48f25c15207fcc6f4e9b
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2021-10-11 14:16:10 +0300

    Update dotnet-nuget-pack.sh

--- scripts/dotnet-nuget-pack.sh
+++ scripts/dotnet-nuget-pack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$TRAVIS_BRANCH" == "main" ]; then
-   cd ..
+   #cd ..
    dotnet pack -p:PackageVersion=1.0.0 --no-restore -o .
    dotnet nuget push *.nupkg --api-key $NYGET_API_KEY --source https://api.nuget.org/v3/index.json
 fi
