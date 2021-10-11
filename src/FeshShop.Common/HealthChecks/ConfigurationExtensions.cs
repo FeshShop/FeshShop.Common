3b929f95bf924b2da634b1d4b362cc89df0a1aed
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2021-10-11 16:44:24 +0300

    add service configuration for health checks of app and database

---
+++ src/FeshShop.Common/HealthChecks/ConfigurationExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace FeshShop.Common.HealthChecks
+{
+    using FeshShop.Common.Mongo;
+    using global::HealthChecks.UI.Client;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+    using Microsoft.AspNetCore.Routing;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+    public static partial class ConfigurationExtensions
+    {
+        public static IServiceCollection AddHealthChecker(this IServiceCollection services, IConfiguration configuration)
+        {
+            var options = configuration
+                .GetSection(nameof(MongoDbSettings))
+                .Get<MongoDbSettings>();
+
+            services
+                .AddHealthChecks()
+                .AddMongoDb(options.ConnectionString, options.Database, HealthStatus.Unhealthy);
+
+            return services;
+        }
+
+        public static void MapHealthCheckPath(this IEndpointRouteBuilder endpoints)
+        {
+            endpoints.MapHealthChecks("/health", new HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+        }
+    }
+}
