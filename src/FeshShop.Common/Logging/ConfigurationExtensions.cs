9e14cb532da26771e2bfcf67d7620af5834f6558
stoyanov7 <georgi.stoyanov777@gmail.com>
2021-11-16 23:01:22 +0200

    configure serilog to write to the console

---
+++ src/FeshShop.Common/Logging/ConfigurationExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace FeshShop.Common.Logging
+{
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Serilog;
+    using Serilog.Events;
+    using System;
+
+    public static partial class ConfigurationExtensions
+    {
+        public static IWebHostBuilder UseLogging(this IWebHostBuilder webHostBuilder, string applicationName = null)
+            => webHostBuilder.UseSerilog((context, loggerConfiguration) => 
+            {
+                var serilogSettings = context
+                    .Configuration
+                    .GetSection(nameof(SerilogSettings))
+                    .Get<SerilogSettings>();
+
+                if (!Enum.TryParse<LogEventLevel>(serilogSettings.Level, true, out var level))
+                    level = LogEventLevel.Information;
+
+                loggerConfiguration
+                    .Enrich.FromLogContext()
+                    .MinimumLevel.Is(level)
+                    .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName);
+
+                if (serilogSettings.ConsoleEnabled)
+                    loggerConfiguration.WriteTo.Console();
+            });
+    }
+}
---
+++ src/FeshShop.Common/Logging/SerilogSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace FeshShop.Common.Logging
+{
+    public class SerilogSettings
+    {
+        public bool ConsoleEnabled { get; set; }
+
+        public string Level { get; set; }
+    }
+}
