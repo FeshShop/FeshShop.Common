5922e92402b83d9d099c37c68496a8d08820639f
stoyanov7 <georgi.stoyanov777@gmail.com>
2021-11-17 12:15:31 +0200

    configure seq, add app settings, set name of project for logging

--- src/FeshShop.Common/Logging/ConfigurationExtensions.cs
+++ src/FeshShop.Common/Logging/ConfigurationExtensions.cs
@@ -16,16 +16,35 @@
                     .GetSection(nameof(SerilogSettings))
                     .Get<SerilogSettings>();
 
+                var seqSettings = context
+                    .Configuration
+                    .GetSection(nameof(SeqSettings))
+                    .Get<SeqSettings>();
+
+                var appSettings = context
+                    .Configuration
+                    .GetSection(nameof(AppSettings))
+                    .Get<AppSettings>();
+
                 if (!Enum.TryParse<LogEventLevel>(serilogSettings.Level, true, out var level))
                     level = LogEventLevel.Information;
+
+                var applicationName = string.Empty;
+
+                if (appSettings != null && string.IsNullOrWhiteSpace(appSettings.Name))
+                    applicationName = appSettings.Name;
 
                 loggerConfiguration
                     .Enrich.FromLogContext()
                     .MinimumLevel.Is(level)
-                    .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName);
+                    .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+                    .Enrich.WithProperty("ApplicationName", applicationName);
 
-                if (serilogSettings.ConsoleEnabled)
+                if (seqSettings != null && serilogSettings.ConsoleEnabled)
                     loggerConfiguration.WriteTo.Console();
+
+                if (seqSettings != null && seqSettings.Enabled)
+                    loggerConfiguration.WriteTo.Seq(seqSettings.Url, apiKey: seqSettings.Token);
             });
     }
 }
---
+++ src/FeshShop.Common/Logging/SeqSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace FeshShop.Common.Logging
+{
+    public class SeqSettings
+    {
+        public bool Enabled { get; set; }
+
+        public string Url { get; set; }
+
+        public string Token { get; set; }
+    }
+}
