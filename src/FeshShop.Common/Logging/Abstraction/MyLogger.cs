00d1fb889f5e153346a8628ef790f591c5eaa50d
stoyanov7 <georgi.stoyanov777@gmail.com>
2021-11-17 16:01:21 +0200

    abstract methods from serilog #1

---
+++ src/FeshShop.Common/Logging/Abstraction/MyLogger.cs
@@ -0,0 +1,19 @@
+﻿namespace FeshShop.Common.Logging.Abstraction
+{
+    using Serilog;
+
+    public class MyLogger : IMyLogger
+    {
+        private readonly ILogger logger;
+
+        public MyLogger(ILogger logger) => this.logger = logger;
+
+        public void Debug(string message) => this.logger.Debug(message);
+
+        public void Error(string message) => this.logger.Error(message);
+
+        public void Info(string message) => this.logger.Information(message);
+
+        public void Warning(string message) => this.logger.Warning(message);
+    }
+}
