cba5a4df0d1de4147e2720f5c4187e2426b68570
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2023-11-06 16:54:24 +0200

    initialize guard class

---
+++ src/FeshShop.Common/Guard/Ensure.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace FeshShop.Common.Guard;
+
+public static class Ensure
+{
+    public static void NotNull(
+        object value,
+        [CallerArgumentExpression(nameof(value))] string paramName = null)
+    {
+        if (value == null) 
+            throw new ArgumentNullException(paramName);
+    }
+
+    public static void NotNullOrWhiteSpace(
+        string value,
+        string message = null,
+        [CallerArgumentExpression(nameof(value))] string paramName = null)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException(message ?? "The value can not be null", paramName);
+    }
+}
